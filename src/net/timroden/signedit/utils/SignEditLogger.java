071db031c75907083e2b5bf258a4c312d0f3b455
Samistine <samuel@samistine.com>
2016-03-22 16:52:47 -0400

    Redo SignEditLogger.java

--- src/net/timroden/signedit/utils/SignEditLogger.java
+++ src/net/timroden/signedit/utils/SignEditLogger.java
@@ -17,55 +17,42 @@ public class SignEditLogger {
 	private SignEdit plugin;
 	private Logger log;
 	private File logFile;
-	private BufferedWriter fileOut;
 	private DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
 	public SignEditLogger(SignEdit plugin) {
 		this.plugin = plugin;
 		this.log = plugin.getLogger();
+
+		this.logFile = new File(this.plugin.getDataFolder(), Config.logName());
+		if (!this.logFile.exists()) {
+			try {
+				this.logFile.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+                        }
+		}
 	}
 
-	public void logAll(String thePlayer, String theCommand, LogType theType,
-			Level level) {
+	public void logAll(String thePlayer, String theCommand, LogType theType, Level level) {
 		String theMessage = thePlayer + ": /signedit " + theCommand;
 
-		if (theType.equals(LogType.PLAYERCOMMAND))
-			theMessage = this.plugin.localization.get("playerCommand") + " "
-					+ thePlayer + ": /signedit " + theCommand;
-		else if (theType.equals(LogType.SIGNCHANGE)) {
-			theMessage = this.plugin.localization.get("signChange") + " "
-					+ thePlayer + theCommand;
+		if (theType.equals(LogType.PLAYERCOMMAND)) {
+			theMessage = this.plugin.localization.get("playerCommand") + " " + thePlayer + ": /signedit " + theCommand;
+                } else if (theType.equals(LogType.SIGNCHANGE)) {
+			theMessage = this.plugin.localization.get("signChange") + " " + thePlayer + theCommand;
 		}
 		if (Config.commandsLogFile()) {
-			logFile("[" + this.dateFormat.format(new Date()) + "] "
-					+ theMessage);
+			logFile("[" + this.dateFormat.format(new Date()) + "] "	+ theMessage);
 		}
-		if (Config.commandsLogConsole())
+		if (Config.commandsLogConsole()) {
 			log(level, theMessage);
+                }
 	}
 
 	private void logFile(String data) {
-		try {
-			openFileOutput();
-			this.fileOut.write(data);
-			this.fileOut.newLine();
-			this.fileOut.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void openFileOutput() {
-		try {
-			this.logFile = new File(this.plugin.getDataFolder(),
-					Config.logName());
-			if (!this.logFile.exists()) {
-				this.logFile.createNewFile();
-			}
-			this.fileOut = new BufferedWriter(new FileWriter(
-					this.plugin.getDataFolder()
-							+ System.getProperty("file.separator")
-							+ Config.logName(), true));
+		try (BufferedWriter fileOut = new BufferedWriter(new FileWriter(logFile, true));){
+			fileOut.write(data);
+			fileOut.newLine();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
